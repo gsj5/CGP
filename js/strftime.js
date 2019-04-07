e8a902c47a91d634e74d5bba53280d7dee6e0929
Tomáš Virtus <tomas.virtus@rchain.coop>
2019-04-07 12:56:59 +0200

    sprintf.js: Use UTC Date methods

--- js/strftime.js
+++ js/strftime.js
@@ -43,68 +43,68 @@ function strftime (fmt, time)
 
 		switch (opt) {
 			case 'a':
-				return days[d.getDay()];
+				return days[d.getUTCDay()];
 			case 'A':
-				return fdays[d.getDay()];
+				return fdays[d.getUTCDay()];
 			case 'b':
-				return months[d.getMonth()];
+				return months[d.getUTCMonth()];
 			case 'B':
-				return fmonths[d.getMonth()];
+				return fmonths[d.getUTCMonth()];
 			case 'c':
 				return d.toLocaleString();
 			case 'd':
-				return pad2(d.getDate());
+				return pad2(d.getUTCDate());
 			case 'H':
-				return pad2(d.getHours());
+				return pad2(d.getUTCHours());
 			case 'I':
-				var hours = d.getHours()%12;
+				var hours = d.getUTCHours()%12;
 				return pad2(hours === 0 ? 12 : hours);
 			case 'j':
-				var d01 = new Date (d.getFullYear(), 0, 1);
-				return pad3(Math.ceil((d.getTime()-d01.getTime())/86400000)+1);
+				var d01 = new Date (d.getUTCFullYear(), 0, 1);
+				return pad3(Math.ceil((d.getUTCTime()-d01.getUTCTime())/86400000)+1);
 			case 'm':
-				return pad2(d.getMonth());
+				return pad2(d.getUTCMonth());
 			case 'M':
-				return pad2(d.getMinutes());
+				return pad2(d.getUTCMinutes());
 			case 'p':
-				return d.getHours() >= 12 ? 'PM' : 'AM';
+				return d.getUTCHours() >= 12 ? 'PM' : 'AM';
 			case 's':
-				return pad2(d.getSeconds());
+				return pad2(d.getUTCSeconds());
 			case 'S':
-				return d.getTime()/1000;
+				return d.getUTCTime()/1000;
 			case 'u':
-				return d.getDay() === 0 ? 7 : d.getDay();
+				return d.getUTCDay() === 0 ? 7 : d.getUTCDay();
 			case 'U':
-				var d01 = new Date(d.getFullYear(),0,1);
-				return pad2(Math.round((Math.ceil((d.getTime()-d01.getTime())/86400000)+1 + 6 - d.getDay())/7));
+				var d01 = new Date(d.getUTCFullYear(),0,1);
+				return pad2(Math.round((Math.ceil((d.getUTCTime()-d01.getUTCTime())/86400000)+1 + 6 - d.getUTCDay())/7));
 			case 'V':
-				var d01 = new Date(d.getFullYear(), 0, 1);
-				var w = Math.round((Math.ceil((d.getTime()-d01.getTime())/86400000)+1 + 7 - (d.getDay() === 0 ? 7 : d.getDay()))/7);
-				var d31 = new Date(d.getFullYear(), 11, 31);
-				if (d01.getDay() < 4 && d01.getDay() > 1) w++;
-				if (w === 53 && d31.getDay() < 4) {
+				var d01 = new Date(d.getUTCFullYear(), 0, 1);
+				var w = Math.round((Math.ceil((d.getUTCTime()-d01.getUTCTime())/86400000)+1 + 7 - (d.getUTCDay() === 0 ? 7 : d.getUTCDay()))/7);
+				var d31 = new Date(d.getUTCFullYear(), 11, 31);
+				if (d01.getUTCDay() < 4 && d01.getUTCDay() > 1) w++;
+				if (w === 53 && d31.getUTCDay() < 4) {
 					w = 1;
 				} else if (w === 0) {
-					d31 = new Date(d.getFullYear()-1, 11, 31);
-					d01 = new Date(d31.getFullYear(), 0, 1);
-					w = Math.round((Math.ceil((d31.getTime()-d01.getTime())/86400000)+1 + 7 - (d31.getDay() === 0 ? 7 : d31.getDay()))/7);
-					if (d01.getDay() < 4 && d01.getDay() > 1) w++;
-					if (w === 53 && d31.getDay() < 4) w = 1;
+					d31 = new Date(d.getUTCFullYear()-1, 11, 31);
+					d01 = new Date(d31.getUTCFullYear(), 0, 1);
+					w = Math.round((Math.ceil((d31.getUTCTime()-d01.getUTCTime())/86400000)+1 + 7 - (d31.getUTCDay() === 0 ? 7 : d31.getUTCDay()))/7);
+					if (d01.getUTCDay() < 4 && d01.getUTCDay() > 1) w++;
+					if (w === 53 && d31.getUTCDay() < 4) w = 1;
 				}
 				return pad2(w);
 			case 'w':
-				return d.getDay();
+				return d.getUTCDay();
 			case 'W':
-				var d01 = new Date(d.getFullYear(),0,1);
-				return pad2(Math.round((Math.ceil((d.getTime()-d01.getTime())/86400000)+1 + 7 - (d.getDay() === 0 ? 7 : d.getDay()))/7));
+				var d01 = new Date(d.getUTCFullYear(),0,1);
+				return pad2(Math.round((Math.ceil((d.getUTCTime()-d01.getUTCTime())/86400000)+1 + 7 - (d.getUTCDay() === 0 ? 7 : d.getUTCDay()))/7));
 			case 'x':
-				return pad2(d.getDate())+'/'+pad2(d.getMonth())+'/'+d.getFullYear();
+				return pad2(d.getUTCDate())+'/'+pad2(d.getUTCMonth())+'/'+d.getUTCFullYear();
 			case 'X':
-				return pad2(d.getHours())+':'+pad2(d.getMinutes())+':'+pad2(d.getSeconds());
+				return pad2(d.getUTCHours())+':'+pad2(d.getUTCMinutes())+':'+pad2(d.getUTCSeconds());
 			case 'y':
-				return pad2(d.getFullYear()%100);
+				return pad2(d.getUTCFullYear()%100);
 			case 'Y':
-				return d.getFullYear();
+				return d.getUTCFullYear();
 			case 'Z':
 				return d.toString().replace(/^.*\(([^)]+)\)$/, '$1');
 			default:
